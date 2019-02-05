6e9c6f90f99af2900534796e0c8ed0850266c0b9
Alan Chan <alanchanzm@outlook.com>
2019-02-05 11:46:53 +0800

    feat(observer): batcher

--- src/observer/batcher.js
+++ src/observer/batcher.js
@@ -1,4 +1,5 @@
-import { nextTick } from '../util/index';
+import { nextTick, warn } from '../util/index';
+import config from '../config';
 
 let queueIndex;
 let queue = [];
@@ -7,11 +8,6 @@ let has = {};
 let circular = {};
 let waiting = false;
 let internalQueueDepleted = false;
-
-// TODO
-function flushBatcherQueue() {
-  throw new Error('TODO');
-}
 
 /**
  * @param watcher { import("./watcher").Watcher }
@@ -32,6 +28,48 @@ function pushWatcher(watcher) {
       nextTick(flushBatcherQueue);
     }
   }
+}
+
+function flushBatcherQueue() {
+  runBatcherQueue(queue);
+  internalQueueDepleted = true;
+  runBatcherQueue(userQueue);
+  resetBatcherState();
+}
+
+/**
+ * 执行队列中的 watcher
+ * @param {Watcher[]} queue
+ */
+function runBatcherQueue(queue) {
+  for (queueIndex = 0; queueIndex < queue.length; queueIndex += 1) {
+    const watcher = queue[queueIndex];
+    const { id } = watcher;
+    has[id] = null;
+    watcher.run();
+
+    if (process.env.NODE_ENV !== 'production' && has[id] !== null) {
+      circular[id] = (circular[id] || 0) + 1;
+      if (circular[id] > config._maxUpdateCount) {
+        warn(
+          `You may have an infinite update loop for watcher with expression "${
+            watcher.expression
+          }"`,
+          watcher.vm
+        );
+        break;
+      }
+    }
+  }
+}
+
+function resetBatcherState() {
+  queue = [];
+  userQueue = [];
+  has = {};
+  circular = {};
+  waiting = false;
+  internalQueueDepleted = false;
 }
 
 export { pushWatcher };
