ee13951ccdc636ff2ef2c3923be912228506503b
Alan Chan <alanchanzm@outlook.com>
2019-02-05 12:13:01 +0800

    feat: nextTick

--- src/util/env.js
+++ src/util/env.js
@@ -1,5 +1,13 @@
+import { isPlainObject } from './lang';
+
 const hasProto = '__proto__' in {};
 
-function nextTick() {}
+const inBrowser = typeof window !== 'undefined' && !isPlainObject(window);
 
-export { hasProto, nextTick };
+function nextTick(nextTickHandler) {
+  // TODO: 利用 MutationObserver 来触发 microtask 执行
+  // 可改为 Promise
+  setTimeout(nextTickHandler, 0);
+}
+
+export { hasProto, inBrowser, nextTick };
