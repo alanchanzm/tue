26a492ef79eddd933deb16a88adc7bcf110821ad
Alan Chan <alanchanzm@outlook.com>
2019-02-12 16:28:52 +0800

    build: perpare for ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,12 @@
 module.exports = {
-  extends: ['airbnb-base', 'prettier'],
+  parser: 'typescript-eslint-parser',
+  plugins: ['typescript'],
+  extends: ['eslint-config-alloy/typescript', 'prettier'],
   env: {
     browser: true,
     node: true,
     es6: true,
   },
-  rules: {
-    'no-bitwise': 0,
-    'import/prefer-default-export': 0,
-    'no-restricted-syntax': 0,
-    'no-underscore-dangle': 0,
-    'no-param-reassign': 0,
-    'no-use-before-define': ['error', { functions: false }],
-    'no-continue': 0,
-    'class-methods-use-this': 0,
-  },
+  root: true,
+  rules: {},
 };
--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,26 @@ const path = require('path');
 module.exports = {
   // __dirname 为当前模块的目录名
   // path.resolve 从右至左拼接地址，直到返回一个绝对路径
-  entry: path.resolve(__dirname, 'src/index.umd.js'),
-  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.ts'),
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'vue.js',
     // library 与 libraryTarget 一同使用
     // 以 umd 的方式暴露模块 Vue，即暴露为所有的模块定义下都可运行的方式
-    library: 'Vue',
-    libraryTarget: 'umd',
+    // library: 'Vue',
+    // libraryTarget: 'umd',
   },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.ts$/, exclude: /node_modules/, loader: 'ts-loader' },
+    ],
+  },
+  resolve: {
+    alias: {
+      '@/': path.resolve(__dirname, 'src/'),
+    },
   },
   // TODO: 监听选项 to be removed
   watch: true,
