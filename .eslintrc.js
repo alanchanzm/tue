7ecf2340fe80ce75159128e46b18a41cd2d6dd69
Alan Chan <alanchanzm@outlook.com>
2019-02-20 19:01:00 +0800

    chore: a demo for explore reactive data

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,23 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
   extends: 'airbnb-base',
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
     // 具名导出优于默认导出：更容易辨识
-    'import/prefer-default-export': 'off'
-  }
+    'import/prefer-default-export': 'off',
+    // 省略箭头函数中不必要的括号
+    'arrow-parens': [2, 'as-needed'],
+    // 允许对函数参数赋值
+    'no-param-reassign': 'off',
+  },
 };
---
+++ exploration/index.js
@@ -0,0 +1,64 @@
+const bindingMark = 'data-vue-binding';
+
+class Vue {
+  constructor(id, initData) {
+    this.bindings = {};
+    this.data = {};
+    this.initData = initData;
+
+    this.el = document.querySelector(`#${id}`);
+    const markToken = this.markToken.bind(this);
+    const content = this.el.innerHTML.replace(/{{(.*)}}/g, markToken);
+    this.el.innerHTML = content;
+
+    this.init();
+  }
+
+  init() {
+    const variables = Object.keys(this.bindings);
+    variables.forEach(variable => this.bind(variable));
+
+    if (this.initData) Object.assign(this.data, this.initData);
+  }
+
+  /**
+   * 为模板添加相应的标记
+   * @param {*} _ useless 占位符
+   * @param {string} variable 模板字符串
+   * @returns {string} <span data-vue-binding="xxx"></span>
+   */
+  markToken(_, variable) {
+    variable = variable.trim();
+    this.bindings[variable] = {};
+    return `<span ${bindingMark}="${variable}"></span>`;
+  }
+
+  /**
+   * 拦截 this.data 的变化，修改对应的 DOM
+   * @param {string} variable 模板字符串
+   */
+  bind(variable) {
+    this.bindings[variable].els = this.el.querySelectorAll(`[${bindingMark}="${variable}"]`);
+    this.bindings[variable].els.forEach(element => element.removeAttribute(bindingMark));
+
+    const get = () => this.bindings[variable].value;
+    const set = newVal => {
+      this.bindings[variable].els.forEach(element => {
+        element.textContent = newVal;
+        this.bindings[variable].value = newVal;
+      });
+    };
+
+    Object.defineProperty(this.data, variable, { get, set });
+  }
+}
+
+const app = new Vue('test', {
+  msg: 'hello',
+});
+
+setTimeout(() => {
+  app.data.what = 'what';
+  app.data.hey = 'hey';
+  app.data.msg = 'goodbye';
+}, 1000);
